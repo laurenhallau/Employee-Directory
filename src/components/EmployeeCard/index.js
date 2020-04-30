6e52957bfaeb299699a1154ac14d503c69bcb419
Lauren Hallau <lhallau@gmail.com>
2020-04-30 14:16:18 -0500

    added url for photos

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -1,23 +1,44 @@
 import React from "react";
+import { Card, CardImg, CardTitle, Row, Col, CardText } from "reactstrap";
 //import "./style.css";
 
+
 function EmployeeCard(props) {
-    console.log("employeecard", props);
     return (
-        <div className="card">
-            <div className="img-container">
-                <img alt={props.name} src={props.image} />
-            </div>
-            <div className="content">
-                <ul>
-                <li>Name: {props.name} </li> 
-                <li>Position: {props.position} </li>
-                <li>Date of Birth: {props.birth} </li>
-                <li>Country: {props.country}</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+        <Row>
+          <Col sm="6">
+            <Card body>
+              <CardTitle>{props.name}</CardTitle>
+              <CardImg top width="100%" src={props.image} />
+              <CardText><ul>
+              <li>Name: {props.name} </li> 
+              <li>Position: {props.position} </li>
+              <li>Date of Birth: {props.birth} </li>
+              <li>Country: {props.country}</li>
+              </ul></CardText>
+            </Card>
+          </Col>
+        </Row>
+      );
+    };
+
+// function EmployeeCard(props) {
+//     console.log("employeecard", props);
+//     return (
+//         <div className="card">
+//             <div className="img-container">
+//                 <img alt={props.name} src={props.image} />
+//             </div>
+//             <div className="content">
+//                 <ul>
+//                 <li>Name: {props.name} </li> 
+//                 <li>Position: {props.position} </li>
+//                 <li>Date of Birth: {props.birth} </li>
+//                 <li>Country: {props.country}</li>
+//                 </ul>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default EmployeeCard;
