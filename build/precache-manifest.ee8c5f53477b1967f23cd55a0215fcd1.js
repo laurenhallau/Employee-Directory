bdda7beb723bc17bd0907b1df7126b4db44e326e
Lauren Hallau <lhallau@gmail.com>
2020-05-02 13:37:16 -0500

    adding a build folder

---
+++ build/precache-manifest.ee8c5f53477b1967f23cd55a0215fcd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddc9533ce3027013c5955abc0c75717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91cc295ed0187bf9be3d",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b53636c5a1f0de2c717c",
+    "url": "/static/css/main.b07bf0ad.chunk.css"
+  },
+  {
+    "revision": "91cc295ed0187bf9be3d",
+    "url": "/static/js/2.a7d7479e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a7d7479e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53636c5a1f0de2c717c",
+    "url": "/static/js/main.7a785f77.chunk.js"
+  },
+  {
+    "revision": "1dfb6a92246aaeb0cb7c",
+    "url": "/static/js/runtime-main.c8d80545.js"
+  }
+]);
