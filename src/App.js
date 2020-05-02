5cb3f5c0cabf28b8efa9f5dcbe9ef8148ea4b902
Lauren Hallau <lhallau@gmail.com>
2020-05-02 12:33:28 -0500

    flex wrap css applied

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Jumbotron from "./components/Title";
 // import EmployeeCard from "./components/EmployeeCard";
 // import Search from "./components/Search";
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import employees from "./employees.json";
 import Main from "./components/Main";
 // import { findRenderedComponentWithType } from 'react-dom/test-utils';
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,25 +1,10 @@
 import React, {Component} from "react";
 import employees from "../../employees.json";
-// import "./style.css";
+import "./style.css";
 // import App from "../../App";
 import EmployeeCard from "../EmployeeCard";
 
-// function Search(props) {
-//     return (
-//         <form className="form-inline justify content center">
-//           <input 
-//           className="form-control mr-sm-2"
-//           type="search"
-//           placeholder="Search"
-//           name={props.name}
-//           value={props.value}
-//           onChange={props.onChange}
-//           label={props.label}
-//           />
-          
-//         </form>
-//     );
-// }
+
 
 class Search extends Component{
   constructor(props){
@@ -30,23 +15,18 @@ class Search extends Component{
     }
   }
   filterList = e => {
-    console.log("working");
     const filterTarget = e.target.value;
     let employeeList = this.state.employees;
-    // console.log(employeeList);
     employeeList = employeeList.filter(i =>{
       let values = Object.values(i)
         .join("")
         .toLocaleLowerCase();
         return values.indexOf(filterTarget.toLowerCase()) !== -1;
-      // console.log(values);
-      // return i.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
     })
     this.setState({filteredEmployees: employeeList});
   }
   
   render(){
-    console.log('fil', this.state.filteredEmployees)
     return(
       <div className="filter-list">
         <input type="text" placeholder="Search" onChange={this.filterList}/>
