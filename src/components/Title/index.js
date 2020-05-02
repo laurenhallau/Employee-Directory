7473d727e6f4cbd5a5cdad267192d0f03ad2e6c9
Lauren Hallau <lhallau@gmail.com>
2020-05-02 16:48:50 -0500

    changed jumbotron title and lead

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -6,8 +6,8 @@ function Title() {
     return (
         <div>
           <Jumbotron className="jumbotron">
-              <h1 className="title">Employee Directory</h1>
-              <p className="lead">Search for an employee by name, position, or country!</p>
+              <h1 className="title">Lola's Dream Fútbol Team!</h1>
+              <p className="lead">Search for a player by name, position, or country!</p>
           </Jumbotron>
         </div>
     );   
